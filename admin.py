131656878ed956754b38babc622f77d53b41751d
JumayevOU <jumayevou@gmail.com>
2025-08-17 14:25:00 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,19 +1,21 @@
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.types import KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def admin_keyboard():
-    builder = ReplyKeyboardBuilder()
-    builder.row(
-        KeyboardButton(text="📢 Barchaga xabar yuborish"),
-        KeyboardButton(text="📨 Userga xabar yuborish")
-    )
-    builder.row(
-        KeyboardButton(text="📊 Statistika"),
-        KeyboardButton(text="🏆 Faol foydalanuvchilar")
-    )
-    builder.row(
-        KeyboardButton(text="📄 Userlar ro'yxati"),
-        KeyboardButton(text="➕ Admin qo'shish")
-    )
-    return builder.as_markup(resize_keyboard=True)
+admin_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='📢 Barchaga xabar yuborish'),
+            KeyboardButton(text='📨 Userga xabar yuborish'),
+        ],
+        [
+            KeyboardButton(text='📊 Statistika'),
+            KeyboardButton(text='🏆 Faol foydalanuvchilar')
+        ],
+        [
+            KeyboardButton(text='📄 Userlar ro\'yxati'),
+            KeyboardButton(text='➕ Admin qo\'shish')
+        ],
+    ], resize_keyboard=True
+)
+
+
 
